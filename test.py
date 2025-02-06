031d4bf922f26c551f6941844c99ce1e0e7a9278
XXXX <XXXXXXX>
2025-02-06 19:25:54 +0900

    天気予報完成

--- test.py
+++ test.py
@@ -1,4 +1,27 @@
 import requests
 import os
-from dotenv
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+# print(api_key)
+
+city = input("天気を調べたい都市を入力してください。 例： 一関市 ")
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=ja&units=metric"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    # print(response)
+
+    weather_data = response.json()
+
+    print(weather_data["weather"][0]["main"])
+    print(weather_data["weather"][0]["description"])
+    print(f"{weather_data['main']['temp']}℃")
+
+else:
+    print("データを取得できませんでしたできませんでした")
+    print(response.text)
